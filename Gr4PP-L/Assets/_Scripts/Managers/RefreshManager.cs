dc842d679521c1a583731b1a945869316158ea6b
Josh Robinson <jorobins@mtu.edu>
2022-02-07 12:49:15 -0500

    2.7.22 v02
    Created a grapple refresher object that can be picked up

---
+++ Gr4PP-L/Assets/_Scripts/Managers/RefreshManager.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/** Author: Josh Robinson
+    * Version 2/7/22
+    */
+
+public class RefreshManager : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
