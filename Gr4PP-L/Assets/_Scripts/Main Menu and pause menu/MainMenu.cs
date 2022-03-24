b397515d52efa719d5188c4d9680e6c755e44687
riverDallas134 <61401928+riverDallas134@users.noreply.github.com>
2022-03-24 14:43:02 -0400

    Added Title Screen and Menu Script

---
+++ Gr4PP-L/Assets/_Scripts/Main Menu and pause menu/MainMenu.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void GameStart()
+        {
+        StartCoroutine(DelayedStart());
+        }
+
+     public void GameOptions()
+            {
+                   StartCoroutine(Controls());
+            }
+
+    public void GameQuit()
+        {
+                Application.Quit(); //quits the game
+        }
+
+    IEnumerator DelayedStart()
+    {
+        yield return new WaitForSeconds(.1f);
+        SceneManager.LoadScene("SampleScene"); //Loads the scene based off of button input
+    }
+    IEnumerator Controls()
+        {
+            yield return new WaitForSeconds(0.1f);
+           SceneManager.LoadScene("Controls"); //Loads the scene based off of button input
+        }
+}
