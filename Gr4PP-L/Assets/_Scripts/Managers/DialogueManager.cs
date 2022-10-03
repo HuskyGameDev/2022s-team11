99e74e2e0bf443df03c5491ff037d63c38e3e8f9
Nick Zimanski <nick.zimanski@gmail.com>
2022-10-03 18:57:52 -0400

    InputManager changes + AudioManager

--- Gr4PP-L/Assets/_Scripts/Managers/DialogueManager.cs
+++ Gr4PP-L/Assets/_Scripts/Managers/DialogueManager.cs
@@ -22,8 +22,15 @@ namespace Managers {
         [SerializeField]
         private float _charsPerSecond = 10f;
 
+        [Tooltip("How long between blinking the '█' character at the end of the text")]
+        public float FullBlockBlinkInterval = 0.5f;
+
         public float Text_secondsPerChar { get => 1f / _charsPerSecond; }
+
+        [HideInInspector]
         public bool Text_incomingNewText;
+
+        private GameManager _gm;
         
         void Awake()
         {
@@ -33,6 +40,7 @@ namespace Managers {
 
         private void Start()
         {
+            _gm = GameManager.Instance;
         }
 
         void Update()
@@ -72,7 +80,8 @@ namespace Managers {
                 return false;
             }
 
-
+            _gm.timerManager.Pause();
+            _gm.inputManager.LockType(InputManager.ControlType.MOVEMENT);
 
             return true;
         }
@@ -91,6 +100,9 @@ namespace Managers {
             Text_incomingNewText = false;
 
             CurrentConversation = null;
+
+            _gm.timerManager.Resume();
+            _gm.inputManager.UnlockType(InputManager.ControlType.MOVEMENT);
         }
 
 
---
+++ Gr4PP-L/Assets/_Scripts/Sound/SoundType.cs
@@ -0,0 +1,7 @@
+namespace Audio {
+    public enum SoundType {
+        DIALOGUE, //Dialogue voices
+        SFX, //Level interactions, menu noises, etc
+        MUSIC //Uh... music
+    }
+}
