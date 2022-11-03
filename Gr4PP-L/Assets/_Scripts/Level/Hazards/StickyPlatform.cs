89cd0e9ca73ee8f91f8f82ac51934bce7ed1acfa
NoahSKol <noahkolcz@gmail.com>
2022-11-03 18:55:43 -0400

    Finished Room 2 and Resetting Moving Plat
    Finished the second room of my level and implemented respawn for moving platforms so that they start in their original state.

--- Gr4PP-L/Assets/_Scripts/Level/Hazards/StickyPlatform.cs
+++ Gr4PP-L/Assets/_Scripts/Level/Hazards/StickyPlatform.cs
@@ -1,79 +1,81 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Level;
 
-public class StickyPlatform : MonoBehaviour
-{
-    public WaypointFollower wf;
+namespace Level {
+    public class StickyPlatform : MonoBehaviour {
+        public WaypointFollower wf;
 
-    private void Awake() {
-        wf = GetComponent<WaypointFollower>();
-    }
+        private void Awake() {
+            wf = GetComponent<WaypointFollower>();
+        }
 
-    //works pretty well, but buffered jumps can mess it up. also the camera doesn't follow it very well, ends up being jittery.
+        //works pretty well, but buffered jumps can mess it up. also the camera doesn't follow it very well, ends up being jittery.
 
-    private void OnTriggerEnter2D(Collider2D collision) {
-        Debug.Log("Trigger Entered");
-        if (collision.gameObject.CompareTag("Player")) {
-            if (!wf.GetActive()) {
+        private void OnTriggerEnter2D(Collider2D collision) {
+            Debug.Log("Trigger Entered");
+            if (collision.gameObject.CompareTag("Player")) {
+                if (!wf.GetActive()) {
+                    collision.gameObject.transform.SetParent(transform);
+                    return;
+                }
+
                 collision.gameObject.transform.SetParent(transform);
+
+                GameObject[] waypoints = wf.GetWaypoints();
+                int currentWaypointIndex = wf.GetCurrentIndex();
+                float speed = wf.GetSpeed();
+
+                if (!wf.GetPaused()) {
+                    Vector2 playerVelocity = collision.gameObject.GetComponent<Rigidbody2D>().velocity;
+                    Vector2 platformVector = waypoints[currentWaypointIndex].transform.position - waypoints[Mathf.Abs((currentWaypointIndex - 1) % waypoints.Length)].transform.position;
+                    Vector2 platformVelocity = platformVector.normalized * speed;
+
+                    collision.gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(playerVelocity.x - platformVelocity.x, playerVelocity.y);
+                } else if (wf.GetBiased()) {
+                    Vector2 playerVelocity = collision.gameObject.GetComponent<Rigidbody2D>().velocity;
+                    Vector2 platformVector = waypoints[Mathf.Abs((currentWaypointIndex - 1) % waypoints.Length)].transform.position - waypoints[Mathf.Abs((currentWaypointIndex - 2) % waypoints.Length)].transform.position;
+                    Vector2 platformVelocity = platformVector.normalized * speed;
+
+                    collision.gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(playerVelocity.x - platformVelocity.x, playerVelocity.y);
+                }
+            }
+        }
+
+        private void OnTriggerExit2D(Collider2D collision) {
+            if (!wf.GetActive()) {
+                collision.gameObject.transform.SetParent(null);
                 return;
             }
+            if (collision.gameObject.CompareTag("Player")) {
+                collision.gameObject.transform.SetParent(null);
 
-            collision.gameObject.transform.SetParent(transform);
+                GameObject[] waypoints = wf.GetWaypoints();
+                int currentWaypointIndex = wf.GetCurrentIndex();
+                float speed = wf.GetSpeed();
 
-            GameObject[] waypoints = wf.GetWaypoints();
-            int currentWaypointIndex = wf.GetCurrentIndex();
-            float speed = wf.GetSpeed();
+                if (!wf.GetPaused()) {
+                    Debug.Log("waypoints.Length: " + waypoints.Length);
+                    Debug.Log("Given index: " + Mathf.Abs((currentWaypointIndex - 1) % waypoints.Length));
+                    Vector2 playerVelocity = collision.gameObject.GetComponent<Rigidbody2D>().velocity;
+                    Vector2 platformVector = waypoints[currentWaypointIndex].transform.position - waypoints[Mathf.Abs((currentWaypointIndex - 1) % waypoints.Length)].transform.position;
+                    Vector2 platformVelocity = platformVector.normalized * speed;
 
-            if (!wf.GetPaused()) {
-                Vector2 playerVelocity = collision.gameObject.GetComponent<Rigidbody2D>().velocity;
-                Vector2 platformVector = waypoints[currentWaypointIndex].transform.position - waypoints[Mathf.Abs((currentWaypointIndex - 1) % waypoints.Length)].transform.position;
-                Vector2 platformVelocity = platformVector.normalized * speed;
+                    collision.gameObject.GetComponent<Rigidbody2D>().velocity = playerVelocity + platformVelocity;
 
-                collision.gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(playerVelocity.x - platformVelocity.x, playerVelocity.y);
-            } else if (wf.GetBiased()) {
-                Vector2 playerVelocity = collision.gameObject.GetComponent<Rigidbody2D>().velocity;
-                Vector2 platformVector = waypoints[Mathf.Abs((currentWaypointIndex - 1) % waypoints.Length)].transform.position - waypoints[Mathf.Abs((currentWaypointIndex - 2) % waypoints.Length)].transform.position;
-                Vector2 platformVelocity = platformVector.normalized * speed;
+                    Debug.Log("Player Velocity: " + playerVelocity);
+                    Debug.Log("Platform Velocity: " + platformVelocity);
+                } else if (wf.GetBiased()) {
+                    Vector2 playerVelocity = collision.gameObject.GetComponent<Rigidbody2D>().velocity;
+                    Vector2 platformVector = waypoints[Mathf.Abs((currentWaypointIndex - 1) % waypoints.Length)].transform.position - waypoints[Mathf.Abs((currentWaypointIndex - 2) % waypoints.Length)].transform.position;
+                    Vector2 platformVelocity = platformVector.normalized * speed;
 
-                collision.gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(playerVelocity.x - platformVelocity.x, playerVelocity.y);
-            }
-        }
-    }
+                    collision.gameObject.GetComponent<Rigidbody2D>().velocity = playerVelocity + platformVelocity;
 
-    private void OnTriggerExit2D(Collider2D collision) {
-        if (!wf.GetActive()) {
-            collision.gameObject.transform.SetParent(null);
-            return;
-        }
-        if (collision.gameObject.CompareTag("Player")) {
-            collision.gameObject.transform.SetParent(null);
-
-            GameObject[] waypoints = wf.GetWaypoints();
-            int currentWaypointIndex = wf.GetCurrentIndex();
-            float speed = wf.GetSpeed();
-
-            if (!wf.GetPaused()) {
-                Debug.Log("waypoints.Length: " + waypoints.Length);
-                Debug.Log("Given index: " + Mathf.Abs((currentWaypointIndex - 1) % waypoints.Length));
-                Vector2 playerVelocity = collision.gameObject.GetComponent<Rigidbody2D>().velocity;
-                Vector2 platformVector = waypoints[currentWaypointIndex].transform.position - waypoints[Mathf.Abs((currentWaypointIndex - 1) % waypoints.Length)].transform.position;
-                Vector2 platformVelocity = platformVector.normalized * speed;
-
-                collision.gameObject.GetComponent<Rigidbody2D>().velocity = playerVelocity + platformVelocity;
-
-                Debug.Log("Player Velocity: " + playerVelocity);
-                Debug.Log("Platform Velocity: " + platformVelocity);
-            } else if (wf.GetBiased()) {
-                Vector2 playerVelocity = collision.gameObject.GetComponent<Rigidbody2D>().velocity;
-                Vector2 platformVector = waypoints[Mathf.Abs((currentWaypointIndex - 1) % waypoints.Length)].transform.position - waypoints[Mathf.Abs((currentWaypointIndex - 2) % waypoints.Length)].transform.position;
-                Vector2 platformVelocity = platformVector.normalized * speed;
-
-                collision.gameObject.GetComponent<Rigidbody2D>().velocity = playerVelocity + platformVelocity;
-
-                Debug.Log("Player Velocity: " + playerVelocity);
-                Debug.Log("Platform Velocity: " + platformVelocity);
+                    Debug.Log("Player Velocity: " + playerVelocity);
+                    Debug.Log("Platform Velocity: " + platformVelocity);
+                }
             }
         }
     }
