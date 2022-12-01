4789a9a47e580f698108c5b7b0c7a52fd9cc63f4
Nick Zimanski <nick.zimanski@gmail.com>
2022-11-30 21:11:08 -0500

    Preparing for playtest!

--- Gr4PP-L/Assets/_Scripts/UI/PromptController.cs
+++ Gr4PP-L/Assets/_Scripts/UI/PromptController.cs
@@ -17,7 +17,7 @@ public class PromptController : MonoBehaviour
 
     void Start()
     {
-        OnInteract = () => {};
+        OnInteract = () => { };
         _im = GameManager.Instance.Get<Managers.InputManager>();
         _promptText = GetComponent<TextMeshProUGUI>();
     }
@@ -30,19 +30,26 @@ public class PromptController : MonoBehaviour
         if (_im.GetButtonDown(_currentInteractAxis)) OnInteract();
     }
 
-    public void AssignInteractable(string axis, Action callback) {
+    public void AssignInteractable(string axis, Action callback)
+    {
         OnInteract += callback;
         _currentInteractAxis = axis;
-        UpdateText("Press " + axis + " to interact");
+        //TODO: Reimplement the commented line, sorry - Nick, 3 hours before playtests.
+        //UpdateText("Press " + axis + " to interact");
+
+        UpdateText("Press ENTER to interact");
+
     }
-    
-    public void UnassignInteractable() {
-        OnInteract = () => {};
+
+    public void UnassignInteractable()
+    {
+        OnInteract = () => { };
         _currentInteractAxis = "";
         UpdateText("");
     }
 
-    private void UpdateText(string str) {
+    private void UpdateText(string str)
+    {
         _promptText.text = str;
     }
 }
